56d6833292940949799112367cb7a5c42cd2c8a9
MUSATVladGabriel <musatvlad24@stud.ase.ro>
2025-10-08 12:24:18 +0300

    Add files via upload

---
+++ ex1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+double medienrint(int nrint[], int dim)
+{
+	int i;
+		double s=0;
+	double m;
+	for (i = 0; i < dim; i++)
+	{
+		s = s + nrint[i];
+	}
+	m = s / dim;
+	return m;
+}
+int main()
+{
+	int nrint[20], dim=4, i;
+	for (i = 0; i < dim; i++)
+		cin >> nrint[i];
+	cout << endl;
+	cout << "Media nr intregi este: ";
+	cout<<medienrint(nrint, 4);
+
+}
